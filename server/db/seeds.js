ecb10a35df509cdc7f671de1acbf3db9821cf9b9
Rebekah <rebekah_7567@hotmail.co.uk>
2018-12-07 13:23:06 +0000

    installed deps and added seeds to db

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,10 +1,32 @@
 use shares_portfolio;
 db.dropDatabase();
 
-db.stocks.insertMany([
-
-]);
+db.stocks;
 
 db.portfolio.insertMany([
-
+  {
+    "companyName": "Ekso Bionics Holdings Inc.",
+    "symbol": "EKSO",
+    "amount": 5
+  },
+  {
+    "companyName": "ESSA Pharma Inc.",
+    "symbol": "EPIX",
+    "amount": 5
+  },
+  {
+    "companyName": "Acceleron Pharma Inc.",
+    "symbol": "XLRN",
+    "amount": 25
+  },
+  {
+    "companyName": "Hanger Inc.",
+    "symbol": "HNGR",
+    "amount": 33
+  },
+  {
+    "companyName": "Daxor Corporation",
+    "symbol": "DXR",
+    "amount": 21
+  }
 ]);

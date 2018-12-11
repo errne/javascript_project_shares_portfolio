1548db643fa0ca5b0b097e032392134e58061506
Rebekah <rebekah_7567@hotmail.co.uk>
2018-12-11 11:57:59 +0000

    started set up for form view file for remove share button

---
+++ client/src/views/stock_form_view.js
@@ -0,0 +1,14 @@
+const StockFormView = function (container, symbol) {
+  this.container = container;
+  this.symbol = symbol;
+};
+
+StockFormView.prototype.render = function () {
+
+};
+
+StockFormView.prototype.renderRemoveButton = function (number_of_shares) {
+
+};
+
+module.exports = StockFormView;
